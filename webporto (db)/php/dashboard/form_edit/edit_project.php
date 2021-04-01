254e620f251bd796b9f8556dafea764815c71b0b
Angkasa Raya <68907186+angkasaraya204@users.noreply.github.com>
2021-04-01 10:36:11 +0700

    Add files via upload

---
+++ webporto (db)/php/dashboard/form_edit/edit_project.php
@@ -0,0 +1,264 @@
+<?php include "../koneksi.php"; ?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="wrappixel, admin dashboard, html css dashboard, web dashboard, bootstrap 5 admin, bootstrap 5, css3 dashboard, bootstrap 5 dashboard, Ample lite admin bootstrap 5 dashboard, frontend, responsive bootstrap 5 admin template, Ample admin lite dashboard bootstrap 5 dashboard template">
+    <meta name="description"
+        content="Ample Admin Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
+    <meta name="robots" content="noindex,nofollow">
+    <title>Edit Project</title>
+    <link rel="canonical" href="https://www.wrappixel.com/templates/ample-admin-lite/" />
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="plugins/images/favicon.png">
+    <!-- Custom CSS -->
+   <link href="css/style.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar" data-navbarbg="skin5">
+            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-header" data-logobg="skin6">
+                <a class="navbar-brand" href="../index.php">
+                        <!-- Logo icon -->
+                        <b class="logo-icon text-center" style="margin-left: 70px;">
+                            <img src="https://i.ibb.co/Mc9tpJq/tb1.png" alt="homepage"/>
+                        </b>
+                    </a>
+                    <a class="nav-toggler waves-effect waves-light text-dark d-block d-md-none"
+                        href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Logo -->
+                <!-- ============================================================== -->
+                <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
+                    <ul class="navbar-nav d-none d-md-block d-lg-none">
+                        <li class="nav-item">
+                            <a class="nav-toggler nav-link waves-effect waves-light text-white"
+                                href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+                        </li>
+                    </ul>
+                    <!-- ============================================================== -->
+                    <!-- Right side toggle and nav items -->
+                    <!-- ============================================================== -->
+                    <ul class="navbar-nav ms-auto d-flex align-items-center">
+
+                        <!-- ============================================================== -->
+                        <!-- Search -->
+                        <!-- ============================================================== -->
+                        <li class=" in">
+                            <form role="search" class="app-search d-none d-md-block me-3">
+                                <input type="text" placeholder="Search..." class="form-control mt-0">
+                                <a href="" class="active">
+                                    <i class="fa fa-search"></i>
+                                </a>
+                            </form>
+                        </li>
+                        <!-- ============================================================== -->
+                        <!-- User profile and search -->
+                        <!-- ============================================================== -->
+                        <?php
+                        $sql = "SELECT * FROM `tb_user` WHERE id";
+                        $query = mysqli_query($connect, $sql);
+                        while($row = mysqli_fetch_array($query)){
+                        ?>
+                        <li>
+                            <a class="profile-pic" href="#">
+                                <img src="<?php echo $row["gambar"];?>" alt="user-img" width="36"
+                                    class="img-circle"><span class="text-white font-medium"><?php echo $row["namalngkp"];?></span></a>
+                        </li>
+                        <?php
+                        }
+                        ?>
+                        <!-- ============================================================== -->
+                        <!-- User profile and search -->
+                        <!-- ============================================================== -->
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                <ul id="sidebarnav">
+                        <!-- User Profile-->
+                        <li class="sidebar-item pt-2">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="./index.php"
+                                aria-expanded="false">
+                                <i class="far fa-clock" aria-hidden="true"></i>
+                                <span class="hide-menu">Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="./profile.php"
+                                aria-expanded="false">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                                <span class="hide-menu">Profile</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="./project.php"
+                                aria-expanded="false">
+                                <i class="fa fa-table" aria-hidden="true"></i>
+                                <span class="hide-menu">Project</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="about.php"
+                                aria-expanded="false">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                                <span class="hide-menu">About</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="service.php"
+                                aria-expanded="false">
+                                <i class="fa fa-wrench" aria-hidden="true"></i>
+                                <span class="hide-menu">Service</span>
+                            </a>
+                        </li>
+                        <li class="text-center p-20 upgrade-btn">
+                            <a href="../login/logout.php"
+                                class="btn d-grid btn-danger text-white">
+                                Logout</a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb bg-white">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Profile page</h4>
+                    </div>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Start Page Content -->
+                <!-- ============================================================== -->
+                <!-- Row -->
+                <div class="row">
+                    <!-- Column -->
+                    <div class="col-lg-4 col-xlg-3 col-md-12">
+                        <div class="white-box">
+                            <div class="user-bg" style="height: 260px;">
+                                <div class="overlay-box">
+                                <?php
+                                    if (isset($_GET['id'])) {
+                                        $id = $_GET['id'];
+                        
+                                        $sql = "SELECT * FROM project WHERE id='$id'";
+                                        $query = mysqli_query($connect, $sql);
+                                        $row = mysqli_fetch_assoc($query);
+                        
+                                        if( mysqli_num_rows($query) > 1 ) {
+                                            die("data tidak ditemukan...");
+                                        }
+                                    }
+                                ?>
+                                <div class="card" style="width: 22rem; height: 260px;">
+                                    <img class="card-img-top" src="<?php echo $row["gambar"];?>" height="150px">
+                                    <div class="card-body">
+                                        <h5 class="card-title" style="text-transform: capitalize;"><?php echo $row["judul"];?></h5>
+                                        <p class="card-text" style="text-transform: capitalize;"><?php echo $row["deskripsi"];?></p>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                    <!-- Column -->
+                    <div class="col-lg-8 col-xlg-9 col-md-12">
+                        
+                        <div class="card">
+                            <div class="card-body">
+                            <?php
+                                    if (isset($_GET['id'])) {
+                                        $id = $_GET['id'];
+                        
+                                        $sql = "SELECT * FROM project WHERE id='$id'";
+                                        $query = mysqli_query($connect, $sql);
+                                        $row = mysqli_fetch_assoc($query);
+                        
+                                        if( mysqli_num_rows($query) > 1 ) {
+                                            die("data tidak ditemukan...");
+                                        }
+                                    }
+                            ?>
+                                <form class="form-horizontal form-material" action="simpan2.php" method="POST">
+                                    <div class="form-group mb-4">
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="hidden" class="form-control p-0 border-0" name="id" value="<?php echo $row["id"];?>"> </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Judul:</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="text" placeholder="Masukan Judul"
+                                                class="form-control p-0 border-0" name="judul" value="<?php echo $row["judul"];?>"> </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Gambar:</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="text" placeholder="Masukan Link"
+                                                class="form-control p-0 border-0" name="gambar" value="<?php echo $row["gambar"];?>"> </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <label for="example-email" class="col-md-12 p-0">Deskripsi:</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="text" placeholder="Masukan Deskripsi"
+                                                class="form-control p-0 border-0"  name="desk" value="<?php echo $row["deskripsi"];?>">
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-4">
+                                        <div class="col-sm-12">
+                                            <input type="submit" name="simpan" class="btn btn-primary" value="Update">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Column -->
+                </div>
+            </div>
+            <footer class="footer text-center"> 2021 © Angkasa Raya | Portofolio
+            </footer>
+        </div>
+    </div>
+    <script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/app-style-switcher.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
